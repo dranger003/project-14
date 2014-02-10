7b3b2c28bfcac2b5746118a9d1fde2b1a59a74e0
Daniel Ranger <dranger003@gmail.com>
2014-02-10 20:17:31 +0000

    Add clock + revert swresample use.

--- main.c
+++ main.c
@@ -73,16 +73,17 @@ int main(int argc, char *argv[])
         printf("%d/%d\n", time_base.num, time_base.den);
         printf("%d/%d\n", frame_rate.num, frame_rate.den);
 
-//        {
-//            res = avformat_seek_file(fc,
-//                                     st_vid_idx,
-//                                     INT64_MIN,
-//                                     atoi(argv[2]),//av_rescale(atoi(argv[2]), fc->streams[st_vid_idx]->time_base.den, fc->streams[st_vid_idx]->time_base.num),
-//                                     INT64_MAX,
-//                                     AVSEEK_FLAG_ANY);
-//            assert(res >= 0);
-//            avcodec_flush_buffers(fc->streams[st_vid_idx]->codec);
-//        }
+        {
+            res = avformat_seek_file(fc,
+                                     st_aud_idx,
+                                     INT64_MIN,
+                                     atoi(argv[2]),//av_rescale(atoi(argv[2]), fc->streams[st_vid_idx]->time_base.den, fc->streams[st_vid_idx]->time_base.num),
+                                     INT64_MAX,
+                                     0);
+//            res = av_seek_frame(fc, st_aud_idx, atoi(argv[2]),0);
+            assert(res >= 0);
+            avcodec_flush_buffers(fc->streams[st_vid_idx]->codec);
+        }
 
 //        AVFormatContext *ofc = NULL;
 //        res = avformat_alloc_output_context2(&ofc, NULL, "alsa", "default");
@@ -118,19 +119,24 @@ int main(int argc, char *argv[])
 
         // AVCODEC_MAX_AUDIO_FRAME_SIZE + FF_INPUT_BUFFER_PADDING_SIZE;
         uint8_t samples[192000 + 16];
-        uint8_t *buf = samples;
+//        uint8_t *buf = samples;
 
-        SwrContext *swr_ctx = swr_alloc();
-        av_opt_set_int(swr_ctx, "in_channel_layout", fc->streams[st_aud_idx]->codec->channel_layout, 0);
-        av_opt_set_int(swr_ctx, "out_channel_layout", fc->streams[st_aud_idx]->codec->channel_layout, 0);
-        av_opt_set_int(swr_ctx, "in_channel_count", fc->streams[st_aud_idx]->codec->channels, 0);
-        av_opt_set_int(swr_ctx, "out_channel_count", fc->streams[st_aud_idx]->codec->channels, 0);
-        av_opt_set_int(swr_ctx, "in_sample_rate", fc->streams[st_aud_idx]->codec->sample_rate, 0);
-        av_opt_set_int(swr_ctx, "out_sample_rate", fc->streams[st_aud_idx]->codec->sample_rate, 0);
-        av_opt_set_sample_fmt(swr_ctx, "in_sample_fmt",  AV_SAMPLE_FMT_FLTP, 0);
-        av_opt_set_sample_fmt(swr_ctx, "out_sample_fmt", AV_SAMPLE_FMT_S16,  0);
-        res = swr_init(swr_ctx);
-        assert(res >= 0);
+//        SwrContext *swr_ctx = swr_alloc();
+//        av_opt_set_int(swr_ctx, "in_channel_layout", fc->streams[st_aud_idx]->codec->channel_layout, 0);
+//        av_opt_set_int(swr_ctx, "out_channel_layout", fc->streams[st_aud_idx]->codec->channel_layout, 0);
+//        av_opt_set_int(swr_ctx, "in_channel_count", fc->streams[st_aud_idx]->codec->channels, 0);
+//        av_opt_set_int(swr_ctx, "out_channel_count", fc->streams[st_aud_idx]->codec->channels, 0);
+//        av_opt_set_int(swr_ctx, "in_sample_rate", fc->streams[st_aud_idx]->codec->sample_rate, 0);
+//        av_opt_set_int(swr_ctx, "out_sample_rate", fc->streams[st_aud_idx]->codec->sample_rate, 0);
+//        av_opt_set_sample_fmt(swr_ctx, "in_sample_fmt",  AV_SAMPLE_FMT_FLTP, 0);
+//        av_opt_set_sample_fmt(swr_ctx, "out_sample_fmt", AV_SAMPLE_FMT_S16,  0);
+//        res = swr_init(swr_ctx);
+//        assert(res >= 0);
+
+        system("setterm -cursor off");
+
+        struct timespec a, b;
+        clock_gettime(CLOCK_MONOTONIC, &a);
 
         while (!quit) {
             av_free_packet(&pkt);
@@ -139,7 +145,7 @@ int main(int argc, char *argv[])
             assert(res == 0);
 
             if (pkt.stream_index == st_aud_idx) {
-                int size;//, i = 0, j, k;
+                int size, i = 0, j, k;
 
                 while (pkt.size > 0) {
                     size = avcodec_decode_audio4(fc->streams[st_aud_idx]->codec, frame, &got_frame, &pkt);
@@ -151,16 +157,16 @@ int main(int argc, char *argv[])
                     if (got_frame) {
                         assert(fc->streams[st_aud_idx]->codec->sample_fmt == AV_SAMPLE_FMT_FLTP);
 
-//                        for (j = 0; j < frame->nb_samples; j++)
-//                            for (k = 0; k < fc->streams[st_aud_idx]->codec->channels; k++)
-//                                ((uint16_t *)samples)[i++] = ((float *)frame->extended_data[k])[j] * SHRT_MAX;
+                        for (j = 0; j < frame->nb_samples; j++)
+                            for (k = 0; k < fc->streams[st_aud_idx]->codec->channels; k++)
+                                ((uint16_t *)samples)[i++] = ((float *)frame->extended_data[k])[j] * SHRT_MAX;
 
-                        res = swr_convert(swr_ctx,
-                                          &buf,
-                                          frame->nb_samples,
-                                          (const uint8_t **)frame->extended_data,
-                                          frame->nb_samples);
-                        assert(res >= 0);
+//                        res = swr_convert(swr_ctx,
+//                                          &buf,
+//                                          frame->nb_samples,
+//                                          (const uint8_t **)frame->extended_data,
+//                                          frame->nb_samples);
+//                        assert(res >= 0);
 
 //                        opkt.size = frame->nb_samples * fc->streams[st_aud_idx]->codec->channels * sizeof(uint16_t);
 //                        opkt.data = samples;
@@ -169,6 +175,18 @@ int main(int argc, char *argv[])
 
 //                        res = av_write_frame(ofc, &opkt);
 //                        assert(res >= 0);
+
+                        long double pts = (long double)pkt.pts / 1000.0;
+                        long double ela = (b.tv_sec - a.tv_sec) + ((long double)(b.tv_nsec - a.tv_nsec) / 1000000000);
+                        long double dif = pts - ela;
+
+                        clock_gettime(CLOCK_MONOTONIC, &b);
+                        fprintf(stdout,
+                                "PTS: %.6Lf, E: %.6Lf, D: %.6Lf              \r",
+                                pts,
+                                ela,
+                                dif);
+                        fflush(stdout);
 
                         ao_play(device,
                                 (char *)samples,
@@ -277,6 +295,8 @@ int main(int argc, char *argv[])
 //                av_frame_unref(frame);
 //            }
         }
+
+        system("setterm -cursor on");
 
 //        av_write_trailer(ofc);
 
